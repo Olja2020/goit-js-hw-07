1d648486366f5e284611f1aa04a18a6b8f083c91
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-02-05 17:34:25 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -3,22 +3,20 @@ const nameUser = document.querySelector("#name-input")
 const output = document.querySelector("#name-output");
 
 nameUser.addEventListener("input", (event) => {
-    nameUser.innerHTML="";
-    const arr=[];
-    arr.push(event);
-    
-
-    for(const event of arr){
-    if(event.data =="" || event.data ==" ") {
+        
+    if(event.data.trim() ==="") {
         
         output.textContent = "Anonymous";
        
     }
     else {
-        nameUser.innerHTML="";
+        
         output.textContent = event.currentTarget.value.trim();
         console.log(event.currentTarget.value.trim()); 
-    }}
+    }
     
   });
-  
+  nameUser.addEventListener("change", () => {
+    nameUser.value = "";
+  });
+
